09646df96349c0c2e6c959932e32b7455218015d
marcfr93 <marcferrater@gmail.com>
2025-02-03 11:34:35 +0100

    Update test_process_dms.py
    Addition of check with Eduard Carbonell and deletion of Davide Laghi (no longer in contract)

--- tests/test_process_dms.py
+++ tests/test_process_dms.py
@@ -3,7 +3,7 @@ from utils.process_dms import parse_DMS
 
 def test_parse_DMS():
     df = parse_DMS()
-    assert df.loc["Davide Laghi", "2024", "July"]["DMS"] == "ATG-NL-RP-PC-F4E-23-00262"
+    assert df.loc["Eduard Carbonell", "2025", "January"]["DMS"] == "ATG-EU-RP-PC-F4E-24-07463"
     assert (
         df.loc["Marc Ferrater", "2024", "August"]["DMS"] == "ATG-NL-RP-PC-F4E-23-00125"
     )
